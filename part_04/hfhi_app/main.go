fbdb53a45685d2dfc859c06a661775ff3222a9b6
Alexandr Plakhotnikov <mxplaha@gmail.com>
2021-01-03 23:53:10 +0300

    in hi app add greeting deutsch

--- part_04/hfhi_app/main.go
+++ part_04/hfhi_app/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "headfirstgo/part_04/hfgreeting"
+import (
+	"headfirstgo/part_04/hfgreeting"
+	"headfirstgo/part_04/hfgreeting/deutsch"
+)
 
 func main() {
 	hfgreeting.Hello()
 	hfgreeting.Hi()
+	deutsch.Hallo()
+	deutsch.GutenTag()
 }
