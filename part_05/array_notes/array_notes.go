febfff9f32695a3d5628564aaab6dd65c1419d03
Alexandr Plakhotnikov <mxplaha@gmail.com>
2021-01-09 00:58:06 +0300

    arrays in golang by example music notes

---
+++ part_05/array_notes/array_notes.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var notes [7]string
+	notes[0] = "do"
+	notes[1] = "re"
+	notes[2] = "mi"
+	fmt.Println(notes[0])
+	fmt.Println(notes[1])
+	fmt.Println(notes[3])
+	fmt.Println(notes[6])
+}
