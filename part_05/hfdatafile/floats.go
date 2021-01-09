a805a8322c2eaebed657cf2878cbcb5737a5c92f
Alexandr Plakhotnikov <mxplaha@gmail.com>
2021-01-10 02:16:28 +0300

    create package datafile. Read data of file and convert array float64

---
+++ part_05/hfdatafile/floats.go
@@ -0,0 +1,35 @@
+// package hfdatafile предназначен для чтения данных из файлов
+package hfdatafile
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+// GetFloats читает значения из каждой строки файла
+func GetFloats(fileName string) ([3]float64, error) {
+	var numbers [3]float64
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	i := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return numbers, err
+		}
+		i++
+	}
+	err = file.Close()
+	if err != nil {
+		return numbers, err
+	}
+	if scanner.Err() != nil {
+		return numbers, scanner.Err()
+	}
+	return numbers, nil
+}
