21c320fc948cb25e2e10571e6c02f13b3b9c478b
Alexandr Plakhotnikov <mxplaha@gmail.com>
2021-01-23 22:24:04 +0300

    create app hfvotecount with segments

---
+++ part_07/hfvotecount_with_segm_app/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"headfirstgo/part_07/hfdtflvote"
+	"log"
+)
+
+func main() {
+	lines, err := hfdtflvote.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var names []string
+	var counts []int
+	for _, line := range lines {
+		matched := false
+		for i, name := range names {
+			if name == line {
+				counts[i]++
+				matched = true
+			}
+		}
+		if matched == false {
+			names = append(names, line)
+			counts = append(counts, 1)
+		}
+	}
+	for i, name := range names {
+		fmt.Printf("%s: %d\n", name, counts[i])
+	}
+}
