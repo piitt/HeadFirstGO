80b5f75dfd12e7308c6c3c6418aba5c6f4edf5cf
Alexandr Plakhotnikov <mxplaha@gmail.com>
2021-02-05 23:16:04 +0300

    part 11 exercise with error assertions try vehicle

---
+++ part_11/exercise_tryvehicle/tryvehicle.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Truck string
+
+type Vehicle interface {
+	Accelerate()
+	Brake()
+	Steer(string)
+}
+
+func (t Truck) Accelerate() {
+	fmt.Println("Speeding up")
+}
+
+func (t Truck) Brake() {
+	fmt.Println("Stopping")
+}
+
+func (t Truck) Steer(direction string) {
+	fmt.Println("Turning", direction)
+}
+
+func (t Truck) LoadCargo(cargo string) {
+	fmt.Println("Loading", cargo)
+}
+
+func TryVehicle(vehicle Vehicle) {
+	vehicle.Accelerate()
+	vehicle.Steer("left")
+	vehicle.Steer("right")
+	vehicle.Brake()
+	truck, ok := vehicle.(Truck)
+	if ok {
+		truck.LoadCargo("test cargo")
+	}
+}
+
+func main() {
+	TryVehicle(Truck("Fnord F180"))
+}
