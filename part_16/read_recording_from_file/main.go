8028361ea4542a256ef521a5baa2f0a4889f2e3f
Alexandr Plakhotnikov <mxplaha@gmail.com>
2021-02-18 00:08:19 +0300

    part 16 read recording from file

---
+++ part_16/read_recording_from_file/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	// если возвращается ошибка, указывающая на то, что файл не существует
+	// вернуть nil вместо сегмента строк
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
+	fmt.Printf("%#v\n", signatures)
+	html, err := template.ParseFiles("view.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+func main() {
+	http.HandleFunc("/guestbook", viewHandler)
+	http.ListenAndServe("localhost:8080", nil)
+}
